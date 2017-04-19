1a1c3b13ae1e4bd190eb79c0695f18901c1cbe97
test2017user <t09723rn@gmail.com>
2017-04-19 15:47:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,40 +15,6 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// ----------ここから----------
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-// ----------ここから----------
-var request = require('request');
-
-app.post('/api', function(req, res) {
-  var options = {
-    method: 'POST',
-    uri: 'https://api.line.me/v2/bot/message/reply',
-    body: {
-      replyToken: req.body.events[0].replyToken,
-      messages: [{
-        type: "text",
-        text: req.body.events[0].message.text
-      }]
-    },　
-    auth: {
-      bearer: 'e4mHF9Qe3wQDGnexQjulQp3KHxJOJ2ZgJIkye7hz7OYaa8gQgJRqoyOKIi+xfJsOnqIby+effXmoUdTVrZE7Jy8/NtXpUB8xvb555aC6Tv/6hWwxe3X50gu+LFdYopMHfdZ4YazvfN44KZDP3obB0gdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
-    },
-    json: true
-  };
-  request(options, function(err, res, body) {
-    console.log(JSON.stringify(res));
-  });
-  res.send('OK');
-});
-// --------ここまで追加--------
-// --------ここまで追加--------
-
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
