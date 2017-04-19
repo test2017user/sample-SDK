1655b1fc5c3727c1fe29be069ae0fe9b54a29b52
test2017user <t09723rn@gmail.com>
2017-04-19 14:52:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,20 +15,6 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
-// create a new express server
-var app = express();
-
 // ----------ここから----------
 var bodyParser = require('body-parser');
 
@@ -40,3 +26,12 @@ app.use(bodyParser.json());
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
